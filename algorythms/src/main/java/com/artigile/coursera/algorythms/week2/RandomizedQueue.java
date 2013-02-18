6363356ec3078ba56c9a424ab627fa86a119feeb
IoaN Bah <ioanbsu1@gmail.com>
2013-02-18 11:05:08 -0800

    INTERNAL: minor fix to satisfy the API.

--- algorythms/src/main/java/com/artigile/coursera/algorythms/week2/RandomizedQueue.java
+++ algorythms/src/main/java/com/artigile/coursera/algorythms/week2/RandomizedQueue.java
@@ -9,7 +9,7 @@ import java.util.Random;
  */
 public class RandomizedQueue<Item> implements Iterable<Item> {
     private static Random random = new Random(System.currentTimeMillis());
-    protected Item[] mainArray;
+    private Item[] mainArray;
     int size = 0;
 
     public RandomizedQueue() {
