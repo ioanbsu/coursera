ce0c1cf528fa1b8dc9172f7de243c07dcfae6deb
ioanbsu <ioanbsu1@gmail.com>
2013-11-05 16:04:57 -0800

    FEATURE: 1st week solution had been implemented.

--- algo2/week1/src/main/java/SAP.java
+++ algo2/week1/src/main/java/SAP.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.LinkedHashSet;
 
 /**
@@ -22,66 +23,148 @@ public class SAP {
         while (!StdIn.isEmpty()) {
             int v = StdIn.readInt();
             int w = StdIn.readInt();
+            Stopwatch stopwatch = new Stopwatch();
             int length = sap.length(v, w);
             int ancestor = sap.ancestor(v, w);
-            StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+//            StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+            StdOut.printf("length = %d, ancestor = %d\n", sap.length(Arrays.asList(v), Arrays.asList(w)), ancestor);
+            System.out.println(stopwatch.elapsedTime());
         }
     }
 
     // length of shortest ancestral path between v and w; -1 if no such path
-    public int length(int v, int w) {
-        LinkedHashSet<Integer> ancesorsA = getAncestors(v);
-        LinkedHashSet<Integer> ancesorsB = getAncestors(w);
-        int ancACounter = 0;
-        int ansBCounter = 0;
-        for (Integer ancesorA : ancesorsA) {
-            ansBCounter = 0;
-            for (Integer ancesorB : ancesorsB) {
-                if (ancesorA.equals(ancesorB)) {
-                    return ancACounter + ansBCounter;
-                }
-                ansBCounter++;
-            }
-            ancACounter++;
-        }
-        return -1;
+    public int length(int a, int b) {
+        return length(Arrays.asList(a), Arrays.asList(b));
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
-    public int ancestor(int v, int w) {
-        LinkedHashSet<Integer> ancesorsA = getAncestors(v);
-        LinkedHashSet<Integer> ancesorsB = getAncestors(w);
-        for (Integer ancesorA : ancesorsA) {
-            for (Integer ancesorB : ancesorsB) {
-                if (ancesorA.equals(ancesorB)) {
-                    return ancesorB;
-                }
-            }
-        }
-        return -1;
+    public int ancestor(int a, int b) {
+        return ancestor(Arrays.asList(a), Arrays.asList(b));
+
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
-    public int length(Iterable<Integer> v, Iterable<Integer> w) {
-        BreadthFirstDirectedPaths breadthFirstDirectedPaths = new BreadthFirstDirectedPaths(digraph, v);
-        int length = Integer.MAX_VALUE;
-        for (Integer node : w) {
-            if (breadthFirstDirectedPaths.hasPathTo(node)) {
-                int newLength = breadthFirstDirectedPaths.distTo(node);
-                if (newLength < length) {
-                    length = newLength;
-                }
+    public int length(Iterable<Integer> a, Iterable<Integer> b) {
+        boolean markedA[] = new boolean[digraph.V()];
+        boolean markedB[] = new boolean[digraph.V()];
+        int distToA[] = new int[digraph.V()];
+        int distToB[] = new int[digraph.V()];
+
+        Queue<Integer> qA = new Queue<Integer>();
+        for (Integer nodeA : a) {
+            markedA[nodeA] = true;
+            distToA[nodeA] = 0;
+            qA.enqueue(nodeA);
+        }
+        Queue<Integer> qB = new Queue<Integer>();
+        for (Integer nodeB : b) {
+            markedB[nodeB] = true;
+            distToB[nodeB] = 0;
+            qB.enqueue(nodeB);
+            if (markedA[nodeB] && markedB[nodeB]) {
+                return 0;
             }
         }
-        if (length == Integer.MAX_VALUE) {
-            return -1;
+        int length = -1;
+        while (true) {
+            boolean counting = false;
+            if (!qA.isEmpty()) {
+                counting = true;
+                int vA = qA.dequeue();
+                for (int w : digraph.adj(vA)) {
+                    if (!markedA[w]) {
+                        distToA[w] = distToA[vA] + 1;
+                        markedA[w] = true;
+                        qA.enqueue(w);
+                    }
+                    if (markedA[w] && markedB[w]) {
+                        length = distToA[w] + distToB[w];
+                        break;
+                    }
+                }
+            }
+            if (!qB.isEmpty()) {
+                counting = true;
+                int vB = qB.dequeue();
+                for (int w : digraph.adj(vB)) {
+                    if (!markedB[w]) {
+                        distToB[w] = distToB[vB] + 1;
+                        markedB[w] = true;
+                        qB.enqueue(w);
+                    }
+                    if (markedA[w] && markedB[w]) {
+                        length = distToA[w] + distToB[w];
+                        break;
+                    }
+                }
+            }
+            if (!counting || length != -1) {
+                break;
+            }
         }
         return length;
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
-    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-        return -1;
+    public int ancestor(Iterable<Integer> a, Iterable<Integer> b) {
+        boolean markedA[] = new boolean[digraph.V()];
+        boolean markedB[] = new boolean[digraph.V()];
+        int distToA[] = new int[digraph.V()];
+        int distToB[] = new int[digraph.V()];
+
+        Queue<Integer> qA = new Queue<Integer>();
+        for (Integer nodeA : a) {
+            markedA[nodeA] = true;
+            distToA[nodeA] = 0;
+            qA.enqueue(nodeA);
+        }
+        Queue<Integer> qB = new Queue<Integer>();
+        for (Integer nodeB : b) {
+            markedB[nodeB] = true;
+            distToB[nodeB] = 0;
+            qB.enqueue(nodeB);
+            if (markedA[nodeB] && markedB[nodeB]) {
+                return nodeB;
+            }
+        }
+        int parentNode = -1;
+        while (true) {
+            boolean counting = false;
+            if (!qA.isEmpty()) {
+                counting = true;
+                int vA = qA.dequeue();
+                for (int w : digraph.adj(vA)) {
+                    if (!markedA[w]) {
+                        distToA[w] = distToA[vA] + 1;
+                        markedA[w] = true;
+                        qA.enqueue(w);
+                    }
+                    if (markedA[w] && markedB[w]) {
+                        parentNode = w;
+                        break;
+                    }
+                }
+            }
+            if (!qB.isEmpty()) {
+                counting = true;
+                int vB = qB.dequeue();
+                for (int w : digraph.adj(vB)) {
+                    if (!markedB[w]) {
+                        distToB[w] = distToB[vB] + 1;
+                        markedB[w] = true;
+                        qB.enqueue(w);
+                    }
+                    if (markedA[w] && markedB[w]) {
+                        parentNode = w;
+                        break;
+                    }
+                }
+            }
+            if (!counting || parentNode != -1) {
+                break;
+            }
+        }
+        return parentNode;
     }
 
     private LinkedHashSet<Integer> getAncestors(int node) {
--- algo2/week1/src/main/java/WordNet.java
+++ algo2/week1/src/main/java/WordNet.java
@@ -1,10 +1,4 @@
-import com.google.common.base.Joiner;
-import com.google.common.base.Stopwatch;
-import com.google.common.io.Files;
-
 import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.*;
 
 /**
@@ -14,31 +8,18 @@ import java.util.*;
  */
 public class WordNet {
 
-    Graph graph;
+    Digraph digraph;
     Map<Integer, Set<String>> synset = new HashMap<Integer, Set<String>>();
-    //    Map<Integer, String> glosses = new HashMap<Integer, String>();
+    Map<Integer, String> synPureData = new HashMap<Integer, String>();
     Map<String, Set<Integer>> nouns = new HashMap<String, Set<Integer>>();
     Map<Integer, Set<Integer>> ancestorsMap = new HashMap<Integer, Set<Integer>>();
+    private SAP sap;
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
         int synSize = buildSynsets(synsets);
         buildHypernyms(hypernyms, synSize);
-        com.google.common.base.Stopwatch stopwatch = new Stopwatch();
-        stopwatch.start();
-        System.out.println(distance("George_Bush", "Jack_Kennedy"));
-        System.out.println(distance("George_Bush", "Eric_Blair"));
-        System.out.println(distance("Minsk", "Homyel"));
-        System.out.println(distance("Moscow", "Homyel"));
-        System.out.println(distance("Moscow", "Vladimir_Putin"));
-        System.out.println(distance("Apostelic_Father", "Vladimir_Putin"));
-        System.out.println(stopwatch);
-        System.out.println(sap("George_Bush", "Jack_Kennedy"));
-        System.out.println(sap("George_Bush", "Eric_Blair"));
-        System.out.println(sap("Minsk", "Homyel"));
-        System.out.println(sap("Moscow", "Homyel"));
-        System.out.println(sap("Moscow", "Vladimir_Putin"));
-        System.out.println(sap("Apostelic_Father", "Vladimir_Putin"));
+        sap = new SAP(digraph);
     }
 
     // for unit testing of this class
@@ -59,104 +40,58 @@ public class WordNet {
 
     // distance between nounA and nounB (defined below)
     public int distance(String nounA, String nounB) {
-        //do bfs here
-        Set<Integer> nounAids = nouns.get(nounA);
-        Set<Integer> nounBids = nouns.get(nounB);
-        if (nounAids.isEmpty() || nounBids.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
-
-        int minDistance = Integer.MAX_VALUE;
-        for (Integer nounBid : nounBids) {
-            for (Integer nounAid : nounAids) {
-                BreadthFirstPaths breadthFirstDirectedPaths = new BreadthFirstPaths(graph, nounAid);
-                int distance = breadthFirstDirectedPaths.distTo(nounBid);
-                if (distance < minDistance) {
-                    minDistance = distance;
-                }
-            }
-        }
-
-        return minDistance;
-
+        return sap.length(nouns.get(nounA), nouns.get(nounB));
     }
 
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB) {
-        Set<Integer> nounAids = nouns.get(nounA);
-        Set<Integer> nounBids = nouns.get(nounB);
-
-        if (nounAids.isEmpty() || nounBids.isEmpty()) {
-            throw new IllegalArgumentException();
-        }
-        int minDistance = Integer.MAX_VALUE;
-        int minNounA = -1;
-        int minNounB = -1;
-        for (Integer nounBid : nounBids) {
-            for (Integer nounAid : nounAids) {
-                BreadthFirstPaths breadthFirstDirectedPaths = new BreadthFirstPaths(graph, nounAid);
-                int distance = breadthFirstDirectedPaths.distTo(nounBid);
-                if (distance < minDistance) {
-                    minDistance = distance;
-                    minNounA = nounAid;
-                    minNounB = nounBid;
-                }
-            }
-        }
-        LinkedHashSet<Integer> ancesorsA = getAncestors(minNounA);
-        LinkedHashSet<Integer> ancesorsB = getAncestors(minNounB);
-        for (Integer ancesorA : ancesorsA) {
-            for (Integer ancesorB : ancesorsB) {
-                if (ancesorA.equals(ancesorB)) {
-                    return Joiner.on(" ").join(synset.get(ancesorA));
-                }
-            }
+        int ancestorId = sap.ancestor(nouns.get(nounA), nouns.get(nounB));
+        if (ancestorId != -1) {
+            return synPureData.get(ancestorId);
         }
         return null;
     }
 
     private int buildSynsets(String synsets) {
-        try {
-            List<String> synsetsFileToStringList = Files.readLines(new File(synsets), Charset.defaultCharset());
-            for (String configStr : synsetsFileToStringList) {
-                String[] values = configStr.split(",");
-                int fieldId = convertIntToInteger(values[0]);
-                Set<String> foundSynset = new HashSet<String>(Arrays.asList(values[1].split(" ")));
-                for (String synonym : foundSynset) {
-                    if (!nouns.containsKey(synonym)) {
-                        nouns.put(synonym, new HashSet<Integer>());
-                    }
-                    nouns.get(synonym).add(fieldId);
+        In in = new In(new File(synsets));
+        while (!in.isEmpty()) {
+            String configStr = in.readLine();
+            String[] values = configStr.split(",");
+            int fieldId = convertIntToInteger(values[0]);
+            String synonyms = values[1];
+            synPureData.put(fieldId, synonyms);
+            Set<String> foundSynset = new HashSet<String>(Arrays.asList(synonyms.split(" ")));
+            for (String synonym : foundSynset) {
+                if (!nouns.containsKey(synonym)) {
+                    nouns.put(synonym, new HashSet<Integer>());
                 }
-                synset.put(fieldId, foundSynset);
+                nouns.get(synonym).add(fieldId);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+            synset.put(fieldId, foundSynset);
         }
+
         return nouns.size();
     }
 
     private void buildHypernyms(String hypernyms, int synSize) {
-        try {
-            List<String> hypernymsFileToStringList = Files.readLines(new File(hypernyms), Charset.defaultCharset());
-            graph = new Graph(synSize);
-            for (String configStr : hypernymsFileToStringList) {
-                String[] values = configStr.split(",");
-                int child = convertIntToInteger(values[0]);
-                createMapIfNecessary(child, ancestorsMap);
-                if (values.length > 1) {
-                    for (int i = 1; i < values.length; i++) {
-                        int parent = convertIntToInteger(values[i]);
-                        graph.addEdge(parent, child);
-                        createMapIfNecessary(parent, ancestorsMap);
-                        ancestorsMap.get(child).add(parent);
-                    }
+        digraph = new Digraph(synSize);
+        In in = new In(new File(hypernyms));
+        while (!in.isEmpty()) {
+            String configStr = in.readLine();
+            String[] values = configStr.split(",");
+            int child = convertIntToInteger(values[0]);
+            createMapIfNecessary(child, ancestorsMap);
+            if (values.length > 1) {
+                for (int i = 1; i < values.length; i++) {
+                    int parent = convertIntToInteger(values[i]);
+                    digraph.addEdge(child, parent);
+                    createMapIfNecessary(parent, ancestorsMap);
+                    ancestorsMap.get(child).add(parent);
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
     }
 
     private LinkedHashSet<Integer> getAncestors(int node) {
