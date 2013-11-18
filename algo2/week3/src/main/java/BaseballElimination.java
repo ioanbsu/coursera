1e11bbe0937b7cff9817d8f8c481bfe2f6673500
ioanbsu <ioanbsu1@gmail.com>
2013-11-17 20:19:34 -0800

    FEATURE: week3 initial project set up

--- algo2/week3/src/main/java/BaseballElimination.java
+++ algo2/week3/src/main/java/BaseballElimination.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Date: 11/17/13
  * Time: 4:24 PM
@@ -7,13 +11,53 @@
 
 public class BaseballElimination {
 
-    // create a baseball division from given filename in format specified below
+    private static final String CONFIG_STRING_SPLITTER = " ";
+    private int[][] gamesLeftMatrix;
+
+    private Team[] teams;
+
+    public static void main(String[] args) {
+        BaseballElimination division = new BaseballElimination(args[0]);
+        for (String team : division.teams()) {
+            if (division.isEliminated(team)) {
+                StdOut.print(team + " is eliminated by the subset R = { ");
+                for (String t : division.certificateOfElimination(team))
+                    StdOut.print(t + " ");
+                StdOut.println("}");
+            } else {
+                StdOut.println(team + " is not eliminated");
+            }
+        }
+    }
+
+    /**
+     * @param filename create a baseball division from given filename in format specified below
+     */
+    public BaseballElimination(String filename) {
+        In in = new In(new File(filename));
+        if (in.isEmpty()) {
+            throw new IllegalArgumentException("Illegal file structure");
+        }
+        int teamsSize = in.readInt();
+        gamesLeftMatrix = new int[teamsSize][teamsSize];
+        teams = new Team[teamsSize];
+
+        int winLossMatrixRow = 0;
+        in.readLine();
+        while (!in.isEmpty()) {
+            teams[winLossMatrixRow] = new Team(in.readString(), in.readInt(), in.readInt(), in.readInt());
+            for (int winLossMatrixCol = 0; winLossMatrixCol < teamsSize; winLossMatrixCol++) {
+                gamesLeftMatrix[winLossMatrixRow][winLossMatrixCol] = in.readInt();
+            }
+            winLossMatrixRow++;
+        }
+    }
 
     /**
      * @return number of teams
      */
     public int numberOfTeams() {
-        return -1;
+        return teams.length;
     }
 
 
@@ -21,7 +65,11 @@ public class BaseballElimination {
      * @return all teams
      */
     public Iterable<String> teams() {
-        return null;
+        List<String> teamsArray = new ArrayList<String>();
+        for (Team team : teams) {
+            teamsArray.add(team.getTeamName());
+        }
+        return teamsArray;
     }
 
     /**
@@ -29,7 +77,7 @@ public class BaseballElimination {
      * @return number of wins for given team
      */
     public int wins(String team) {
-        return -1;
+        return getTeamByName(team).getWins();
     }
 
     /**
@@ -37,7 +85,7 @@ public class BaseballElimination {
      * @return number of losses for given team
      */
     public int losses(String team) {
-        return -1;
+        return getTeamByName(team).getLoss();
     }
 
     /**
@@ -45,7 +93,7 @@ public class BaseballElimination {
      * @return number of remaining games for given team
      */
     public int remaining(String team) {
-        return -1;
+        return getTeamByName(team).getLeft();
     }
 
     /**
@@ -54,7 +102,9 @@ public class BaseballElimination {
      * @return number of remaining games between team1 and team2
      */
     public int against(String team1, String team2) {
-        return -1;
+        int team1Index = getTeamIndexInArray(team1);
+        int team2Index = getTeamIndexInArray(team2);
+        return gamesLeftMatrix[team1Index][team2Index];
     }
 
     /**
@@ -62,14 +112,121 @@ public class BaseballElimination {
      * @return is given team eliminated?
      */
     public boolean isEliminated(String team) {
+        int teamIndexInArray = getTeamIndexInArray(team);
+        int numberOfTeamsButOne = numberOfTeams() - 1;
+        int gamesBetweenTeams = (numberOfTeamsButOne - 1) * numberOfTeamsButOne / 2;
+        int totalVertices = 2 + gamesBetweenTeams + numberOfTeamsButOne;
+        FlowNetwork teamsFlowNetwork = getTeamsFlowNetwork(teamIndexInArray, numberOfTeamsButOne, gamesBetweenTeams, totalVertices);
+        new FordFulkerson(teamsFlowNetwork, 0, totalVertices - 1);
+        for (FlowEdge flowEdge : teamsFlowNetwork.adj(0)) {
+            if (flowEdge.capacity() != flowEdge.flow()) {
+                return true;
+            }
+        }
         return false;
     }
 
+
     /**
-     * @param team the team name
+     * @param teamStr the team name
      * @return subset R of teams that eliminates given team; null if not eliminated
      */
-    public Iterable<String> certificateOfElimination(String team) {
+    public Iterable<String> certificateOfElimination(String teamStr) {
+        if (!isEliminated(teamStr)) {
+            return null;
+        }
+        int teamIndexInArray = getTeamIndexInArray(teamStr);
+        int numberOfTeamsButOne = numberOfTeams() - 1;
+        int gamesBetweenTeams = (numberOfTeamsButOne - 1) * numberOfTeamsButOne / 2;
+        int totalVertices = 2 + gamesBetweenTeams + numberOfTeamsButOne;
+        FlowNetwork teamsFlowNetwork = getTeamsFlowNetwork(teamIndexInArray, numberOfTeamsButOne, gamesBetweenTeams, totalVertices);
+        List<String> certificateOfElimination = new ArrayList<String>();
+
+        for (FlowEdge flowEdge : teamsFlowNetwork.adj(0)) {
+            if (flowEdge.capacity() != 0) {
+
+            }
+        }
+        for (int i = 0; i < teams.length; i++) {
+            if (!teams[i].getTeamName().equals(teamStr)) {
+                certificateOfElimination.add(teams[i].getTeamName());
+            }
+        }
+        return certificateOfElimination;
+    }
+
+
+    private int getTeamIndexInArray(String teamName) {
+        for (int i = 0; i < teams.length; i++) {
+            if (teams[i].getTeamName().equals(teamName)) {
+                return i;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private Team getTeamByName(String teamName) {
+        int teamIndex = getTeamIndexInArray(teamName);
+
+        if (teamIndex != -1) {
+            return teams[teamIndex];
+        }
         return null;
+    }
+
+
+    private FlowNetwork getTeamsFlowNetwork(int teamIndexInArray, int numberOfTeamsButOne, int gamesBetweenTeams, int totalVertices) {
+        FlowNetwork teamsFlowNetwork = new FlowNetwork(totalVertices);
+
+        int nextVertexIndex = 1;
+        for (int i = 0; i < numberOfTeamsButOne; i++) {
+            int matrixI = i >= teamIndexInArray ? i + 1 : i;
+            for (int j = i + 1; j < numberOfTeamsButOne; j++) {
+                int matrixJ = j >= teamIndexInArray ? j + 1 : j;
+                teamsFlowNetwork.addEdge(new FlowEdge(0, nextVertexIndex, gamesLeftMatrix[matrixI][matrixJ]));
+                teamsFlowNetwork.addEdge(new FlowEdge(nextVertexIndex, gamesBetweenTeams + i + 1, Double.POSITIVE_INFINITY));
+                teamsFlowNetwork.addEdge(new FlowEdge(nextVertexIndex, gamesBetweenTeams + j + 1, Double.POSITIVE_INFINITY));
+                nextVertexIndex++;
+            }
+        }
+        for (int secondLevelIndex = 0; secondLevelIndex < numberOfTeamsButOne; secondLevelIndex++) {
+            int rightTeamIndex = secondLevelIndex >= teamIndexInArray ? secondLevelIndex + 1 : secondLevelIndex;
+            int edgeCapacity = Math.max(0, teams[teamIndexInArray].getWins() + teams[teamIndexInArray].getLeft() - teams[rightTeamIndex].getWins());
+            teamsFlowNetwork.addEdge(new FlowEdge(gamesBetweenTeams + 1 + secondLevelIndex, totalVertices - 1, edgeCapacity));
+        }
+        return teamsFlowNetwork;
+    }
+
+    private class Team {
+        private String teamName;
+
+        private int wins;
+
+        private int loss;
+
+        private int left;
+
+        private Team(String teamName, int wins, int loss, int left) {
+            this.teamName = teamName;
+            this.wins = wins;
+            this.loss = loss;
+            this.left = left;
+        }
+
+        public String getTeamName() {
+            return teamName;
+        }
+
+        public int getWins() {
+            return wins;
+        }
+
+        public int getLoss() {
+            return loss;
+        }
+
+        public int getLeft() {
+            return left;
+        }
     }
 }
