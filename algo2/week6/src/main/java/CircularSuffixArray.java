42a02decb30d0cf5fee4a14aa21d80cea12bc4f5
Ivan Bahdanau <ibahdanau@fitbit.com>
2013-12-07 09:57:27 -0800

    INTERNAL: dev space is ready fot week6

---
+++ algo2/week6/src/main/java/CircularSuffixArray.java
@@ -0,0 +1,29 @@
+/**
+ * @author ivanbahdanau
+ */
+public class CircularSuffixArray {
+
+    /**
+     * circular suffix array of s
+     *
+     * @param s str
+     */
+    public CircularSuffixArray(String s) {
+
+    }
+
+    /**
+     * @return length of s
+     */
+    public int length() {
+        return -1;
+    }
+
+    /**
+     * @param i the index
+     * @return returns index of ith sorted suffix
+     */
+    public int index(int i) {
+        return -1;
+    }
+}
