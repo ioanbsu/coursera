7306c21c2d9aba02a58125fb9827fc656656b17b
Ivan Bahdanau <ibahdanau@fitbit.com>
2013-12-07 22:00:18 -0800

    FEATURE: Move To Front and CirculatSuffixArray had been implemented.

--- algo2/week6/src/main/java/CircularSuffixArray.java
+++ algo2/week6/src/main/java/CircularSuffixArray.java
@@ -3,20 +3,35 @@
  */
 public class CircularSuffixArray {
 
+    private int[] indexArray;
+
     /**
      * circular suffix array of s
      *
      * @param s str
      */
     public CircularSuffixArray(String s) {
+        int stringLength = s.length();
+        char[] charArray = s.toCharArray();
+        indexArray = new int[stringLength];
+        SuffixArray[] sortedSuffixes = new SuffixArray[stringLength];
+        for (int i = 0; i < s.length(); i++) {
+            char[] sortedArray = new char[stringLength];
+            System.arraycopy(charArray, i, sortedArray, 0, stringLength - i);
+            System.arraycopy(charArray, 0, sortedArray, stringLength - i, i);
+            sortedSuffixes[i] = new SuffixArray(sortedArray);
+            indexArray[i] = i;
 
+        }
+        sort(sortedSuffixes);
+//        System.out.println("");
     }
 
     /**
      * @return length of s
      */
     public int length() {
-        return -1;
+        return indexArray.length;
     }
 
     /**
@@ -24,6 +39,85 @@ public class CircularSuffixArray {
      * @return returns index of ith sorted suffix
      */
     public int index(int i) {
-        return -1;
+        return indexArray[i];
+    }
+
+
+    public static void main(String[] args) {
+        new CircularSuffixArray("ABRACADABRA!");
+    }
+
+
+    private void sort(Comparable[] array) {
+        sort(array, 0, array.length - 1);
+        //printArray(array);
+    }
+
+    private void sort(Comparable[] array, int compareInd1, int compareInd2) {
+        if (compareInd1 >= compareInd2) {
+            return;
+        }
+        int found = sort(array, compareInd1, compareInd2, false);
+//        printArray(array);
+//        System.out.println(found);
+        sort(array, compareInd1, found - 1);
+        sort(array, found + 1, compareInd2);
+    }
+
+    private int sort(Comparable[] array, int compareInd1, int compareInd2, boolean searchUp) {
+        if (compareInd1 >= compareInd2) {
+            return compareInd2;
+        }
+        if (!searchUp) {
+            for (int i = compareInd2; i > compareInd1; i--) {
+                if (array[i].compareTo(array[compareInd1]) < 0) {
+                    exch(array, compareInd1, i);
+                    return sort(array, compareInd1 + 1, i, true);
+                }
+            }
+            return compareInd1;
+        } else {
+            for (int i = compareInd1; i < compareInd2; i++) {
+                if (array[i].compareTo(array[compareInd2]) > 0) {
+                    exch(array, compareInd2, i);
+                    return sort(array, i, compareInd2 - 1, false);
+                }
+            }
+            return compareInd2;
+        }
+    }
+
+
+    private void exch(Comparable[] array, int compareInd1, int compareInd2) {
+        Comparable el = array[compareInd1];
+        array[compareInd1] = array[compareInd2];
+        array[compareInd2] = el;
+        int tmp = indexArray[compareInd1];
+
+        indexArray[compareInd1] = indexArray[compareInd2];
+        indexArray[compareInd2] = tmp;
+
+
+    }
+
+    private class SuffixArray implements Comparable<SuffixArray> {
+
+        private char[] array;
+
+        private SuffixArray(char[] array) {
+            this.array = array;
+        }
+
+        @Override
+        public int compareTo(SuffixArray otherArray) {
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] > otherArray.array[i]) {
+                    return 1;
+                } else if (array[i] < otherArray.array[i]) {
+                    return -1;
+                }
+            }
+            return 0;
+        }
     }
 }
--- algo2/week6/src/main/java/MoveToFront.java
+++ algo2/week6/src/main/java/MoveToFront.java
@@ -2,18 +2,40 @@
  * @author ivanbahdanau
  */
 public class MoveToFront {
+
+
+    private static final int ASCII_SIZE = 256;
+    private static char[] ascii = new char[ASCII_SIZE];
+
     /**
      * apply move-to-front encoding, reading from standard input and writing to standard output
      */
     public static void encode() {
-
+        for (int i = 0; i < ascii.length; i++) {
+            ascii[i] = (char) i;
+        }
+        for (int i = 0; !BinaryStdIn.isEmpty(); i++) {
+            char c = BinaryStdIn.readChar();
+            int replaceCharacter = updateArray(c);
+            BinaryStdOut.write(replaceCharacter, 8);
+        }
+        BinaryStdOut.close();
     }
 
     /**
      * apply move-to-front decoding, reading from standard input and writing to standard output
      */
     public static void decode() {
-
+        for (int i = 0; i < ascii.length; i++) {
+            ascii[i] = (char) i;
+        }
+        for (int i = 0; !BinaryStdIn.isEmpty(); i++) {
+            char c = BinaryStdIn.readChar(8);
+            char character = ascii[c];
+            BinaryStdOut.write(character, 8);
+            updateArray(character);
+        }
+        BinaryStdOut.close();
     }
 
     /**
@@ -23,6 +45,28 @@ public class MoveToFront {
      * @param args args
      */
     public static void main(String[] args) {
-
+        String value = args[0];
+        if ("-".equals(value)) {
+            encode();
+        }
+        if ("+".equals(value)) {
+            decode();
+        }
     }
+
+
+    private static int updateArray(char c) {
+        int replaceCharacter = -1;
+        for (int searchCharIndex = 0; searchCharIndex < ASCII_SIZE; searchCharIndex++) {
+            if (ascii[searchCharIndex] == c) {
+                replaceCharacter = searchCharIndex;
+                break;
+            }
+        }
+        System.arraycopy(ascii, 0, ascii, 1, replaceCharacter);
+        ascii[0] = c;
+        return replaceCharacter;
+    }
+    //java -classpath "/Users/ivanbahdanau/IdeaProjects/git/coursera/libs/algs4.jar:/Users/ivanbahdanau/IdeaProjects/git/coursera/libs/stdlib.jar:" MoveToFront - < abra.txt | java -classpath "/Users/ivanbahdanau/IdeaProjects/git/coursera/libs/algs4.jar:/Users/ivanbahdanau/IdeaProjects/git/coursera/libs/stdlib.jar:" MoveToFront +
+
 }
